187a345491f573aaf2650842f9181af091550c56
Rain <pyc0422@gmail.com>
2024-07-25 19:11:27 -0500

    delete cashonly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
       <div className='text-xl'>Toyko Sushi Menu</div>
-      <div className='text-lg'>Cash Only</div>
       <div>
         Pick Up Address:1320 W 32nd St
       </div>
