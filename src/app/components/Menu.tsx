d1384c56240c2d64d0984aeb82a6e670291b815c
Rain <pyc0422@gmail.com>
2024-07-25 20:04:02 -0500

    add logo

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 const Menu:React.FC = () => {
   const images = ['menu1', 'menu2', 'price1', 'price2'];
   return (
-    <div>
-      {images.map(img => <Image src={`/images/${img}.jpg`} key={img} alt="menu1" height={500} width={500}/>)}
+    <div className="my-4">
+      <div className="text-xl text-center mb-2">Menu | 菜单</div>
+      {images.map(img => <Image src={`/images/${img}.jpg`} key={img} alt="menu1" height={300} width={500} priority />)}
 
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image';
 import Menu from './components/Menu';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
-      <div className='text-xl'>Toyko Sushi Menu</div>
+      <div className='text-xl flex flex-row justify-center items-center'>
+        <Image src="/images/logo.png" height={30} width={30} className='w-auto p-2' alt="logo"/>
+        <div>Toyko Sushi Depot</div>
+      </div>
       <div>
-        Pick Up Address:1320 W 32nd St
+        Pick-up Address ｜ 取货地址: 1320 W 32nd St
       </div>
       <Menu />
     </main>
